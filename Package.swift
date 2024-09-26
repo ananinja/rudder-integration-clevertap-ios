fdabe2c7b9845a06455e48563d1af883586daf63
numandev1 <muhammadnuman70@gmail.com>
2024-09-26 16:50:54 +0500

    feat: add swift package support

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "RudderCleverTap",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "RudderCleverTap",
+            targets: ["RudderCleverTap"]
+        )
+    ],
+    dependencies: [
+        // Rudder SDK
+        .package(url: "https://github.com/rudderlabs/rudder-sdk-ios.git", from: "1.12.0"),
+        // CleverTap SDK
+        .package(url: "https://github.com/CleverTap/clevertap-ios-sdk.git", from: "6.2.1"),
+    ],
+    targets: [
+        .target(
+            name: "RudderCleverTap",
+            dependencies: [
+                .product(name: "Rudder", package: "rudder-sdk-ios"),
+                .product(name: "CleverTapSDK", package: "clevertap-ios-sdk")
+            ],
+            path: "Rudder-CleverTap/Classes",
+            exclude: ["Example"],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Rudder-CleverTap/Classes")
+            ]
+        )
+    ]
+)
